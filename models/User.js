d3938f94d74765e0673b4de262f6d5938cacdd90
Khaja Prasad Abburu <khajaprasad@gmail.com>
2024-09-02 00:37:23 +0530

    working backend
    backed working properly until now

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile_photo_url: {
+      type: String,
+      default:
+        "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=338&ext=jpg&ga=GA1.1.1819120589.1724630400&semt=ais_hybrid",
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+module.exports = mongoose.model("User", userSchema);
